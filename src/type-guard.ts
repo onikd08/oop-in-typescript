5555e9c061675bd1a095c405e6d4eed7e1e96ec1
Anik Das <onikd08@gmail.com>
2023-05-05 05:48:07 +0300

    Using in operator for narrowing

--- src/type-guard.ts
+++ src/type-guard.ts
@@ -22,3 +22,30 @@ function getType(param: checkType): string {
 }
 
 console.log(getType("false")); // output: string
+
+// in Operator guard
+
+interface INormalUserAccount {
+  name: string;
+  age: number;
+}
+
+interface IAdminUserAccount {
+  name: string;
+  age: number;
+  role: "admin";
+}
+
+const isAdmin = (
+  userAccount: INormalUserAccount | IAdminUserAccount
+): boolean => {
+  if ("role" in userAccount) {
+    return !!userAccount.role;
+  } else {
+    return false;
+  }
+};
+const adminUser1: IAdminUserAccount = { name: "Admin", age: 40, role: "admin" };
+const normalUser1: INormalUserAccount = { name: "User 1", age: 30 };
+
+console.log(isAdmin(normalUser1));
